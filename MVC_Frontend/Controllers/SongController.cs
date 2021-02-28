56adcdee01771c5c4ce690f9d1bed3312def43bc
ganeshmurthy2703 <ganeshmurthy2027@gmail.com>
2021-02-28 18:39:49 -0500

    MVC FrontEnd created

---
+++ MVC_Frontend/Controllers/SongController.cs
@@ -0,0 +1,49 @@
+﻿using MVC_Frontend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+//controller for Songs
+namespace MVC_Frontend.Controllers
+{
+    public class SongController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public SongController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Method to return Song1 view
+        public IActionResult Song1()
+        {
+            return View();
+        }
+
+        //Method to return Song2 view
+        public IActionResult Song2()
+        {
+            return View();
+        }
+
+        //Method to return Song3 view
+        public IActionResult Song3()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_Frontend/README.md
@@ -0,0 +1,10 @@
+# WebAPI_MVC_React app
+
+## About 
+This readme provides a general overview of how to run the entire project.
+
+## How to run
+- Navigate to the SongsAPI folder, open command prompt from there, then type ```dotnet run``` command to start the server that serves the songs lyrics.
+- Then Navigate to the MVC_Frontend folder, open command prompt from there, then type ```dotnet run``` command to start the ASP .NET core MVC application to run and fetch data from api server.
+- Navigate to the react_frontend folder, open command prompt there, install dependencies by running ```npm install``` command, and then run ```npm start``` command to run react app to fetch data from server.
+
